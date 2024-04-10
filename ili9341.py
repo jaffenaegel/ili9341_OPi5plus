0e0cff79f1729441be7f0016711fcb746fd09c59
Jaffe De Naegel <150960820+jaffenaegel@users.noreply.github.com>
2024-04-11 00:46:01 +0200

    Update ili9341.py with image example

--- ili9341.py
+++ ili9341.py
@@ -1,5 +1,7 @@
 import ctypes
 import time
+from PIL import Image
+import numpy as np
 
 
 class ILI9341:
@@ -14,6 +16,9 @@ class ILI9341:
         self.__test = lib.Test
         self.__test.argtypes = [ctypes.c_int]
         self.__test.restype = ctypes.c_double
+
+        self.__sending_image = lib.DisplayImage
+        self.__sending_image.argtypes = [ctypes.POINTER(ctypes.c_uint8), ctypes.c_int]
 
     @property
     def background_led_enabled(self):
@@ -30,17 +35,34 @@ class ILI9341:
 
     def test(self, number_of_frames=60):
         """
-        Displays two colors alternating rapidly for the number of frames provided.
-        Then it calculates how many frames were displayed per second on average and returns that number.
+        Displays two colors alternating rapidly for the number of frames provided. Then calculates how many frames were displayed per second on average
+        and returns that number.
         """
         if type(number_of_frames) != int:
             raise TypeError("Argument 'number_of_frames' must be of type int")
         return self.__test(number_of_frames)
 
+    def __send_bytes(self, data):
+        self.__sending_image((ctypes.c_uint8 * len(data))(*data), len(data))
+
+    def send_image(self, image):
+        data = self.__image_to_bytes(image)
+        self.__send_bytes(data)
+
+    @staticmethod
+    def __image_to_bytes(image, rotation=0):
+        pb = np.rot90(image, (rotation // 90) - 1).astype("uint16")
+        red = (pb[..., [0]] & 0xF8) << 8
+        green = (pb[..., [1]] & 0xFC) << 3
+        blue = (pb[..., [2]] & 0xF8) >> 3
+        result = red | green | blue
+        return result.byteswap().tobytes()
+
 
 if __name__ == "__main__":
+    img = Image.open("cat.jpg")
     ili = ILI9341()
     print(ili.test())
-    ili.background_led_enabled = False
     time.sleep(1)
-    print(ili.background_led_enabled)
+    ili.send_image(img)
+    time.sleep(1)
